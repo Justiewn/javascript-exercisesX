949fa3fa3a8fdfb0e02122b4db6b9f263107fcc4
Justin W <justinwei.dev@gmail.com>
2021-04-01 22:13:09 +1300

    completed reverseString and removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    myList = arguments[0];
+    console.log(myList);
+    numItemsRemovable = arguments.length;
+    for (i = 1; i < numItemsRemovable; i++) {
+        let index = myList.indexOf(arguments[i]);
+        if (index > -1) {
+            myList.splice(index,1);
+        }
+    }
+    return myList;
 }
 
 module.exports = removeFromArray
