795a47988266eadd3528e6ec6adc68d3004df6ff
Justin W <justinwei.dev@gmail.com>
2021-04-01 22:15:49 +1300

    completed reverseString and removeFromArray

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let split = string.split("");
+    let reverse = split.reverse();
+    let answer = reverse.join("");
+    return answer;
 }
 
 module.exports = reverseString
