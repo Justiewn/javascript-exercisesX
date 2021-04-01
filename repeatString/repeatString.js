9acb43d86a23d696d8cab8f0b4a3a6b55c7b5afb
Justin W <justinwei.dev@gmail.com>
2021-04-01 21:48:44 +1300

    completed helloWord and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n < 0) return 'ERROR';
+    let finalString = '';
+    for (i = 0; i < n; i++) {
+        finalString += string;
+    }
+    return finalString;
 }
 
 module.exports = repeatString
