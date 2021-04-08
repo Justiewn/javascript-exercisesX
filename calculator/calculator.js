0f5d5703b83beaf716127672f613114547928a48
Justin W <justinwei.dev@gmail.com>
2021-04-08 17:22:07 +1200

    completed calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,29 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce( (total, num) => total += num, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce( (total, num) => total *= num, 1);
 }
 
-function power() {
-	
+function power(x, n) {
+	return x ** n;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	let total = 1;
+	for (i = 1; i <= num; i++) {
+		total *= i;
+	}
+	return total;
 }
 
 module.exports = {
