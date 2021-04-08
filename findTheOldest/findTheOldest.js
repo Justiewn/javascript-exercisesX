ddb1bb80e91b20968bf1a230df456730a76c54fc
Justin W <justinwei.dev@gmail.com>
2021-04-08 19:56:05 +1200

    completed findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(array) {
+    return array.sort( (a, b) => {
+        if (!a.yearOfDeath) a.yearOfDeath = 2020;
+        if (!b.yearOfDeath) b.yearOfDeath = 2020;
+        aAge = a.yearOfDeath - a.yearOfBirth;
+        bAge = b.yearOfDeath - b.yearOfBirth;
+        return (aAge < bAge) ? 1 : -1;
+    })[0];
 }
 
 module.exports = findTheOldest
