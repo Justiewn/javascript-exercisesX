f00a1afd3c4151726098a454c7463790935b6591
Justin W <justinwei.dev@gmail.com>
2021-04-08 19:22:21 +1200

    completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS';
+    if (n == 1) return 1;
+    let a = 0;
+    let b = 1;
+    let temp;
+    for (i = 0; i < n-1; i++) {
+        temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 
 }
 
