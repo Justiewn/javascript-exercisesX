d3affd50472bde84aa2c3592919c85d7cae9dfa4
Justin W <justinwei.dev@gmail.com>
2021-04-08 17:53:52 +1200

    completed palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    const letters = /^[a-z]+$/;
 
+    let stringLower = string.toLowerCase();
+
+    let toArray = Array.from(stringLower);
+
+    let stripped = toArray.filter( ch => (ch.match(letters)) );
+    let strippedReverse = stripped.slice().reverse();  // reverse() is IN-PLACE
+
+    if (stripped.join() == strippedReverse.join()) return true;
+    return false;
 }
 
 module.exports = palindromes
